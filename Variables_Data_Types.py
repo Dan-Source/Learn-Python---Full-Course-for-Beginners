4a78fb3cbc56d215c37f17eb37360ccfd69fd1aa
Dan-source <dan.dluis.dl@gmail.com>
2020-01-30 16:19:36 -0300

    Variables and Data types

---
+++ Variables_Data_Types.py
@@ -0,0 +1,9 @@
+character_name = "Jonh"
+character_age = 70
+is_male = True
+
+
+print("There once was a man named "+ character_name +",")
+print("he has {} years old.".format( character_age))
+print("He really like the name "+ character_name +", ")
+print("but didn't like being {} .".format( character_age))
