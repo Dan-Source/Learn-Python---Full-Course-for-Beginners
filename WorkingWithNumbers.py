9c6fe11b2ac87224e536f7672e1e102c4020a982
Dan-source <dan.dluis.dl@gmail.com>
2020-02-02 16:00:03 -0300

    Curso de Python

---
+++ WorkingWithNumbers.py
@@ -0,0 +1,50 @@
+# Adicao
+
+print(2 + 5)
+
+# subtracao
+
+print(2 - 5)
+
+# multiplicacao 
+
+print(9 * 9)
+
+# divisao 
+
+print(10 / 5)
+
+
+# especificar a ordem das operacoes
+
+print(3 * (4 + 5))
+
+# mod
+
+print(10 % 3)
+
+# converter em um str
+my_num = 5
+print(type(str(my_num)))
+
+# raiz quadrada
+
+print(pow(5, 2))
+
+# escolher o maior
+
+print(max(5, 2))
+
+# min
+
+print(min(5, 2))
+
+# arrendondar
+
+print(round(5.2))
+
+# importar funcoes matematicas
+
+from math import *
+
+print(sqrt(9))
---
+++ WorkingWithString.py
@@ -0,0 +1,29 @@
+phrase = "Corrente Piaui"
+
+# Concatenacao
+print(phrase + " e maravilhoso. ")
+
+# Para alterar a String podemos usar funcoes:
+
+print(phrase.upper())
+
+# Tambem podemos realizar combincao com as funcoes
+
+print(phrase.upper().isupper())
+
+# Saber a quantidade de caracteres contidos em uma String
+
+print(len(phrase))
+
+
+# especificar um caractere contido em uma String
+
+print(phrase[13])
+
+# ou uma index de caracter especifico 
+
+print(phrase.index("i"))
+
+# substituir um pedaco da string
+
+print(phrase.replace("Corrente Piaui", "Alto Fogoso"))
